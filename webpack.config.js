428a289890e55e402c518a6069d780ff5bcdf81c
ArenF <hjh040@gmail.com>
2024-06-22 13:42:22 +0900

    webpack react typescript 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,61 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-        {
-            test: /\.jsx?/,
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
-        },
-        {
-            test: /.css?$/,
-            exclude: [],
-            use: ["style-loader", "css-loader"],
-        },
-    ]
-  },
-  devServer: {
-    host: 'localhost',
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-    open: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-    new Dotenv({
-        path: '.env'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'static' }
-      ]
-    })
-  ],
-};
+const port = process.env.PORT || 3000;
+
+const config = {
+    mode:'development',
+    entry:'./src/index.tsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: port,
+        open: true,
+        host: "localhost", 
+    },
+    plugins: [
+		new HtmlWebpackPlugin({
+            // 번들링시 그대로 가져갈 html 파일
+          	// 해당 html 파일에는 react 가 렌더링할 HTMLElement 가 있어야한다.
+			template: "public/index.html",
+		})
+	],
+	module: {
+		rules: [
+			{
+              	// typscript를 webpack이 이해하게 한다.
+				test: /\.(ts|tsx)$/i,
+				loader: "ts-loader",
+				exclude: ["/node_modules/"],
+			},
+			{
+              	// css에 대한 설정
+				test: /\.css$/i,
+				use: ["css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                // 번들링시 파일을 base64로 인코딩 하지 않고 파일째로 나오게함
+                // "url-loader" 플러그인이 필요하지 않게 됨!
+				type: "asset/resource",
+                generator: {
+                    // 번들링 된 파일을 이름 그대로 생성함
+					filename: "public/[name][ext]",
+				},
+			},
+		],
+	},
+	resolve: {
+      	// 중요! tsconfig 와 경로가 같아야한다.
+		alias: {
+			src: path.resolve(__dirname, "src"),
+			public: path.resolve(__dirname, "public"),
+		},
+		extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+	},
+}
+
+module.exports = () => {
+    return config;
+}
