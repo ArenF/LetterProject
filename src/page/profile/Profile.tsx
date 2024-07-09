935e613c25afe8f6b2cd9029a7ceed59a8a2d4a9
ArenF <hjh040@gmail.com>
2024-07-10 02:22:15 +0900

    프로필 친구 요청 UI 수정 및 정리

--- src/page/profile/Profile.tsx
+++ src/page/profile/Profile.tsx
@@ -1,125 +1,10 @@
 import { CheckCircleIcon, SearchIcon } from "@chakra-ui/icons";
 import { Avatar, Box, Button, Card, CardBody, CardHeader, IconButton, Input, InputGroup, InputRightElement, SkeletonCircle, SkeletonText, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { sendFriendsRequest } from "src/firestore/friendsRequest";
-import { allProfiles, getProfile, ProfileData } from "src/firestore/profileDB";
 import { LoginState } from "src/reducer/login";
-
-type ProfileElementType = {
-    uid: string,
-    name: string,
-    photoUrl: string,
-};
-
-const ProfileElement = (
-    { uid, name, photoUrl }:ProfileElementType
-):JSX.Element => {
-
-    const loginData = useSelector<any, LoginState>((state) => state.login);
-
-    return (
-        <Stack
-            direction="row"
-            align='center'
-            spacing={8}
-            justify='stretch'
-            position="relative"
-        >
-            <Stack
-                position="relative"
-                w="50%"
-                h="100%"
-                direction="row"
-                align='center'
-                justify='stretch'
-            >
-                <Avatar 
-                    src={photoUrl}
-                />
-                <Text
-                    size={"lg"}
-                >{name}</Text>
-            </Stack>
-            <Stack
-                position="relative"
-                w="50%"
-                h="100%"
-                direction="row-reverse"
-                align='center'
-                justify='stretch'
-            >
-                <IconButton 
-                    variant='outline'
-                    aria-label="friend request"
-                    icon={<CheckCircleIcon bgSize={10}/>}
-                    color='green'
-                    onClick={() => {
-
-                    }}
-                />
-            </Stack>
-        </Stack>
-    );
-};
-
-const ProfileList = ():JSX.Element => {
-
-    const [profiles, setProfiles] = useState([]);
-
-    useEffect(() => {
-        allProfiles((list) => {
-            list.forEach((value:ProfileData, index:number) => {
-                console.log(`id : ${index} => ${value}`);
-            })
-            setProfiles(list);
-        });
-    }, []);
-
-    useEffect(() => {
-        profiles.forEach((profile:ProfileData) => {
-            const name = profile.name;
-            const url = URL.createObjectURL(profile.photo);
-
-            console.log(`${name} => ${url}`);
-        });
-    }, [profiles]);
-
-    if (profiles.length === 0) {
-        return (
-            <Stack
-                paddingTop={4}
-                align='stretch'
-                direction="column"
-            >
-                <SkeletonCircle size='10' />
-                <SkeletonText spacing={1} skeletonHeight='1' />
-            </Stack>
-        );
-    } else {
-        return (
-            <Stack
-                paddingTop={4}
-                align='stretch'
-                direction="column"
-            >
-                {profiles.map((value:ProfileData, index) => (
-                    <ProfileElement 
-                        key={index + 1}
-                        uid={value.uid}
-                        name={value.name}
-                        photoUrl={URL.createObjectURL(value.photo)}
-                    />
-                ))}
-    
-            </Stack>
-        );
-    }
-};
+import { FriendRequestList } from "./FriendRequest";
 
 const TabRow = ():JSX.Element => {
-
-    const [search, setSearch] = useState('');
 
     const tabElements = [
         {
@@ -141,27 +26,16 @@ const TabRow = ():JSX.Element => {
         {
             name: "친구",
             content: (
-                <Box></Box>
+                <Box>
+
+                </Box>
             )
         },
         {
             name: "친구 요청",
             content: (
                 <Box>
-                    <InputGroup>
-                        <Input 
-                            value={search}
-                            onChange={(e) => { setSearch(e.target.value) }}
-                        />
-                        <InputRightElement>
-                            <IconButton 
-                                aria-label="Search Friends"
-                                icon={<SearchIcon/>}
-                            />
-                        </InputRightElement>
-                    </InputGroup>
-
-                    <ProfileList />
+                    <FriendRequestList />
                 </Box>
             )
         }
@@ -217,8 +91,9 @@ const Profile = ():JSX.Element => {
                                 fontWeight="bold"
                                 fontSize="large"
                                 color="gray.600"
-                            >{loginData.name}</Text>
-                            
+                            >
+                                {loginData.name}
+                            </Text>
                         </Stack>
                     </Stack>
                 </CardHeader>
