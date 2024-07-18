240d6e69f87cdce3716b6bd0667663cd15106b79
ArenF <hjh040@gmail.com>
2024-07-18 20:06:14 +0900

    날짜 지정 기능 추가, 시간 지정 기능 추가

--- src/page/letter/LetterCreate.tsx
+++ src/page/letter/LetterCreate.tsx
@@ -1,16 +1,45 @@
-import { CalendarIcon } from "@chakra-ui/icons";
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Editable, EditableInput, EditablePreview, Flex, IconButton, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Portal, Text, Textarea, useEditableControls } from "@chakra-ui/react";
+import { ArrowLeftIcon, ArrowRightIcon, CalendarIcon } from "@chakra-ui/icons";
+import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Editable, EditableInput, EditablePreview, Flex, IconButton, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger, Portal, Text, Textarea, useEditableControls } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import TitleEditable from "src/component/editable/TitleEditable";
 import LetterSideNav from "src/component/navbar/LetterSideNav";
 import DraggableSticker from "src/component/stickers/DraggableSticker";
 import { LetterState } from "src/reducer/letter";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import { useState } from "react";
+import moment from "moment";
 
 const LetterCreator = () => {
 
     const letterData = useSelector<any, LetterState>((state) => state.letter);
 
     const stickers = letterData.stickers;
+
+    const [date, setDate] = useState(new Date());
+    const [page, setPage] = useState(0);
+
+    const body = [
+        {
+            name: '날짜 설정',
+            content: (
+                <Calendar 
+                    onChange={(value:Date) => {
+                        setDate(value);
+                    }}
+                    value={date}
+                    calendarType="gregory"
+                    formatDay={(locale, date) => moment(date).format("DD")}
+                />
+            ),
+        },
+        {
+            name: '시간 설정',
+            content: (
+                <Box></Box>
+            ),
+        }
+    ];
 
     return (
         <Box
@@ -73,15 +102,66 @@ const LetterCreator = () => {
                                 />
                             </PopoverTrigger>
                             <Portal>
-                                <PopoverContent>
-                                    <PopoverArrow />
-                                    <PopoverCloseButton />
+                                <PopoverContent bgColor='blue.800'>
+                                    <PopoverArrow bg='blue.800' />
+                                    <PopoverCloseButton color='white' />
                                     <PopoverHeader>
-                                        <Text>보낼 날짜</Text>
+                                        <Text color='white'>
+                                            {body[page].name}
+                                        </Text>
                                     </PopoverHeader>
                                     <PopoverBody>
-                                        
+                                        {body[page].content}
                                     </PopoverBody>
+                                    <PopoverFooter
+                                        border='0'
+                                        display='flex'
+                                        alignItems='center'
+                                        justifyContent='space-between'
+                                        pb={4}
+                                    >
+                                        <Box>
+                                            <Text
+                                                color='white'
+                                            >
+                                                Step {page+1} of {body.length}
+                                            </Text>
+                                        </Box>
+                                        <ButtonGroup>
+                                            <IconButton 
+                                                aria-label="go to previous"
+                                                icon={<ArrowLeftIcon/>}
+                                                color='white'
+                                                bg='blue.800'
+                                                _hover={{
+                                                    bgColor:'blue.700'
+                                                }}
+                                                onClick={() => {
+                                                    const prev = page-1;
+                                                    if (prev < 0) {
+                                                        return;
+                                                    }
+                                                    setPage(prev);
+                                                }}
+                                            />
+                                            <IconButton 
+                                                aria-label="go to next"
+                                                icon={<ArrowRightIcon/>}
+                                                color='white'
+                                                bg='blue.800'
+                                                _hover={{
+                                                    bgColor:'blue.700'
+                                                }}
+                                                onClick={() => {
+                                                    const next = page+1;
+                                                    if (next > body.length-1) {
+                                                        return;
+                                                    }
+                                                    setPage(next);
+                                                }}
+                                            />
+                                        </ButtonGroup>
+                                    </PopoverFooter>
                                 </PopoverContent>
                             </Portal>
                         </Popover>
